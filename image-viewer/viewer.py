f18a8077d118ffef2e932d0ac7e2e9cc86ae24b8
mickael <mickael.bourgois@epitech.eu>
2018-03-19 18:20:36 +0100

    Rebased json branch onto master

--- image-viewer/viewer.py
+++ image-viewer/viewer.py
@@ -219,7 +219,7 @@ def createFifo():
             return -1
     return 0
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     app = App()
     # app.style = ttk.Style()
     # #('clam', 'alt', 'default', 'classic')

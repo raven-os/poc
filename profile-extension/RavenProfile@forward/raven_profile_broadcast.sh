645becc65216a8b3fdc5cca980a8bceb060fce87
mickael <mickael.bourgois@epitech.eu>
2018-03-23 04:09:05 +0100

    Added dock position changements on profile extension

--- profile-extension/RavenProfile@forward/raven_profile_broadcast.sh
+++ profile-extension/RavenProfile@forward/raven_profile_broadcast.sh
@@ -4,3 +4,9 @@ for entry in `ls /raven_com`; do
     echo -n "$1" > "/raven_com/$entry" &
 done
 gsettings set org.gnome.desktop.background picture-uri file://$HOME/.local/share/gnome-shell/extensions/RavenProfile@forward/b$1.jpg
+
+[ "$1" = "1" ] && gsettings set org.gnome.shell.extensions.dash-to-dock dock-position LEFT;
+
+[ "$1" = "2" ] && gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM;
+
+[ "$1" = "3" ] && gsettings set org.gnome.shell.extensions.dash-to-dock dock-position RIGHT;

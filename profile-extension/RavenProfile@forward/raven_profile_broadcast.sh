1230849e97e164aab0045f33130f98260f5491dc
mickael <mickael.bourgois@epitech.eu>
2018-03-23 03:53:34 +0100

    Added background changes on profile extension

--- profile-extension/RavenProfile@forward/raven_profile_broadcast.sh
+++ profile-extension/RavenProfile@forward/raven_profile_broadcast.sh
@@ -3,3 +3,4 @@
 for entry in `ls /raven_com`; do
     echo -n "$1" > "/raven_com/$entry" &
 done
+gsettings set org.gnome.desktop.background picture-uri file://$HOME/.local/share/gnome-shell/extensions/RavenProfile@forward/b$1.jpg
